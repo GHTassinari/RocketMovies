575cda20a53d9b8abb8d281e898b0cac5ed40836
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-15 01:56:06 -0300

    Created the Input component

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,11 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Container } from './styles';
+
+export function SignIn(){
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
