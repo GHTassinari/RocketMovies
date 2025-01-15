97ffd7d467feaed508dcdf5b26355f938c42830f
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-15 01:36:37 -0300

    Created the styles using styled-component

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+    COLORS: {
+        BACKGROUND: "#1C1B1E",
+        BACKGROUND_BLACK: "#0D0C0F",
+        BACKGROUND_GRAY: "#312E38",
+        BACKGROUND_PINK: "#FF859C0D",
+
+        WHITE: "#FFFFFF",
+        BLACK: "#000000",
+        
+        INPUT_PLACEHOLDER: "#948F99",
+        INPUT_BACKGROUND: "#262529",
+
+        GRAY_300: "#999591",
+        GRAY_100: "#CAC4CF",
+
+
+        PINK: "#FF859B",
+    }
+}
