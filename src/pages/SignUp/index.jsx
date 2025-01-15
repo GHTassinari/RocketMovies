f55e0e017f39dc3b2e5915d36a60416a357b3d72
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-15 02:38:10 -0300

    Created the SignUp page

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,33 @@
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Background } from "./styles";
+
+export function SignUp() {
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Application to keep track of everything you watch.</p>
+
+        <h2>Create your account</h2>
+
+        <Input placeholder="Name" type="text" icon={FiUser} />
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Password" type="password" icon={FiLock} />
+
+        <Button title="Sign up" />
+
+        <Link to="/">
+          Already have an account?
+        </Link>
+      </Form>
+    </Container>
+  );
+}
